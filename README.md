842d7fbed1678cab991c9aff0f5d7fcba72f13a9
Adnan <adnanvaktapuriya@gmail.com>
2025-04-19 16:18:58 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# US States Game
+
+This project is a Python-based game that helps users learn and memorize the 50 states of the United States. The game uses a graphical interface to display a blank map of the US, and users are prompted to type in the names of states. Correct answers are displayed on the map, while incorrect answers are ignored.
+
+## Files
+
+- **main.py**: The main script that runs the game.
+- **50_states.csv**: A CSV file containing the names and coordinates of all 50 states.
+- **blank_states_img.gif**: An image file of a blank US map used as the game's background.
+
+## How to Run
+
+1. Ensure you have Python installed on your system.
+2. Install the required libraries by running:
+   ```
+   pip install -r requirements.txt
+   ```
+3. Run the game using the command:
+   ```
+   python main.py
+   ```
+
+## Requirements
+
+This project requires the following Python libraries:
+- `turtle`
+- `pandas`
+
+## License
+
+This project is licensed under the MIT License.
---
+++ main.py
@@ -0,0 +1,55 @@
+import turtle
+import pandas as pd
+import time
+
+FONT = ("Times New Roman",8,'normal')
+
+screen = turtle.Screen()
+screen.title("Name The US States")
+
+image = "blank_states_img.gif"
+
+screen.addshape(image)
+turtle.shape(image)
+
+t = turtle.Turtle()
+t.hideturtle()
+t.penup()
+
+runtime = 60 * 10 #60 secs * 10 for 10 minutes
+close_time = time.time() + runtime
+
+with open("50_states.csv") as state_data:
+    data = pd.read_csv(state_data)
+
+state_names = data.state.to_list()
+
+#x_michigan = data[data.state == "Michigan"].x
+#print(x_michigan)
+
+game_on = True
+correct = 0
+guessed_states = []
+
+while game_on:
+    user_answer = screen.textinput(title=f"{correct}/50 Correct | Guess the next state",prompt="What's another state name?")
+    for state in state_names:
+        if (user_answer.lower() not in guessed_states) and (user_answer.lower() == state.lower()):
+            correct += 1
+            guessed_states.append(state.lower())
+            x_cor = int(data[data.state == user_answer.title()].x)
+            y_cor = int(data[data.state == user_answer.title()].y)
+            t.goto(x_cor,y_cor)
+            t.write(arg= f"{user_answer.title()}",font=FONT,align="center")
+
+    #Chkecking if user guessed all 50 states
+    if correct == 50:
+        game_on = False
+        screen.bye()
+
+    #Checking if the timer ran out
+    if time.time() >= close_time:
+        game_on = False
+        screen.bye()
+
+print(f"\n\nThanks for playing the game. Final score = {correct}/50.\n")
